ec2212c22146b1c41af6597565e509957444ca2d
ebresie <ebresie@gmail.com>
2019-11-02 10:52:49 -0500

    updated lambda examples

--- src/main/com/bresie/dev/java/whatsnew/java8/language/lambda/LambdaExpressions.java
+++ src/main/com/bresie/dev/java/whatsnew/java8/language/lambda/LambdaExpressions.java
@@ -7,12 +7,23 @@ package com.bresie.dev.java.whatsnew.java8.language.lambda;
  * They enable you to treat functionality as a method argument, or code as data.
  * Lambda expressions let you express instances of single-method interfaces
  * (referred to as functional interfaces) more compactly.
+
+some of these examples are 
+basef on from "Java 8 Lambdas" by Richard Warburton - example 2-3.
  *
  */
 public class LambdaExpressions {
     public static void main(String[] args) {
-        // some of these examples are taken from "Java 8 Lambdas" by Richard Warburton
-        Runnable noArguments =  () -> System.out.println("Hello World");
-        noArguments.run();
+        noArgumentExample();
     }
+    
+/* this example defines a. onargument lambda 
+associated with a Runnable interface varable by
+which has a Runnable.run() interface which 
+can be called to execute.
+*/
+void noArgumentExample() {
+Runnable noArguments =  () -> System.out.println("Hello World");
+        noArguments.run();
+}
 }
