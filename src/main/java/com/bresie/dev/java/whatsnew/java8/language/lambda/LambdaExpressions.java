5b24d373a78c34aedb1d42bdc0f35202b6deca8c
Eric Bresie <ebresie@gmail.com>
2019-11-02 11:53:34 -0500

    Updates to include additional examples on

--- src/main/java/com/bresie/dev/java/whatsnew/java8/language/lambda/LambdaExpressions.java
+++ src/main/java/com/bresie/dev/java/whatsnew/java8/language/lambda/LambdaExpressions.java
@@ -1,4 +1,9 @@
-package com.bresie.dev.java.whatsnew.java8.language.lambda;
+package main.java.com.bresie.dev.java.whatsnew.java8.language.lambda;
+
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.function.BinaryOperator;
 
 /**
  * From https://www.oracle.com/technetwork/java/javase/8-whats-new-2157071.html
@@ -14,6 +19,7 @@ package com.bresie.dev.java.whatsnew.java8.language.lambda;
 public class LambdaExpressions {
     public static void main(String[] args) {
         noArgumentExample();
+        withArgumentExample();
     }
 
     /* this example defines a no argument lambda associated with a Runnable interface variable by
@@ -22,5 +28,60 @@ public class LambdaExpressions {
     public static void noArgumentExample() {
         Runnable noArguments = () -> System.out.println("Hello World");
         noArguments.run();
+
+        Runnable noArgumentsMultipleStatesments = () -> {
+            System.out.println("Hello ");
+            System.out.println("Multi-statement lambda ");
+            System.out.println("World");
+        };
+
+        noArgumentsMultipleStatesments.run();
+    }
+
+    /* This example defines a lambda with arguments.  It also shows the BinaryOperator with a type defined
+    which is similar to how Generics specify things.  This implies the return value expected as part of the
+    operator.
+
+    There are multiple examples one with no types associated with the parameter and in another with the parameter
+    included.
+    */
+    public static void withArgumentExample() {
+        BinaryOperator<Long> add = (x, y) -> x + y;
+        BinaryOperator<Long> addExplicit = (Long x, Long y) -> x + y;
+
+        System.out.println("Add=" + add.apply(1L, 2L));
+        System.out.println("AddExplicit=" + addExplicit.apply(3L, 4L));
+    }
+
+    /**
+     * This is an old way of using anonymous inner classes prior to lambdas
+     */
+    public static void oldWay() {
+        final String name = getUserName();
+        JButton button = new JButton();
+        button.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                System.out.println("Hi " +name);
+            }
+        });
+    }
+    /**
+     * This is an old way of using anonymous classes prior to lambdas
+     */
+    public static void newWay() {
+        // with java 8 it is less stringent on finalization of variables when being used in lambda contexts
+        // compare to anonymous inner
+        String name = getUserName();
+        JButton button = new JButton();
+        button.addActionListener(event -> {
+            System.out.println("Hi " + name);
+        });
+    }
+
+    private static String getUserName() {
+        String userName = "John Doe";
+
+        return userName;
     }
 }
