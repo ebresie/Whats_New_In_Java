f924af2a395045d3d56cafdfb996f32e1f141aa9
Eric Bresie <ebresie@gmail.com>
2020-12-11 13:12:31 -0600

    Update RepeatingAnnotations.java

--- src/main/java/com/bresie/dev/java/whatsnew/java8/language/annotations/RepeatingAnnotations.java
+++ src/main/java/com/bresie/dev/java/whatsnew/java8/language/annotations/RepeatingAnnotations.java
@@ -1,5 +1,7 @@
 /**
  * This will provide an example of RepeatingAnnotations usage.
+ * (4) Repeating Annotations provide the ability to apply the same annotation 
+ * type more than once to the same declaration or type use.
  */
 class RepeatingAnnotations {
   // TODO: Implement examples
