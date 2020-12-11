641f73f46ec55cd22668c6c061f28c55ff0f0796
Eric Bresie <ebresie@gmail.com>
2020-12-11 13:13:40 -0600

    Update DefaultMethods.java

--- src/main/java/com/bresie/dev/java/whatsnew/java8/language/defaultMethods/DefaultMethods.java
+++ src/main/java/com/bresie/dev/java/whatsnew/java8/language/defaultMethods/DefaultMethods.java
@@ -1,5 +1,7 @@
 /**
  * This will be an example of Default methods usage provided with java 8
+ *
+ * (2) Method references provide easy-to-read lambda expressions for methods that already have a name.
  */
  class DefaultMethods {
     // TODO: Add example code
