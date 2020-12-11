1c32daa00d58089313e25f70d89143cb2cefb6a2
Eric Bresie <ebresie@gmail.com>
2020-12-11 13:11:52 -0600

    Update TypeAnnotations.java

--- src/main/java/com/bresie/dev/java/whatsnew/java8/language/annotations/TypeAnnotations.java
+++ src/main/java/com/bresie/dev/java/whatsnew/java8/language/annotations/TypeAnnotations.java
@@ -1,5 +1,8 @@
 /**
  * This will contain example for associated with Type Annotations
+ * 
+ * (5) Type Annotations provide the ability to apply an annotation anywhere a type is used, 
+ * not just on a declaration. Used with a pluggable type system, this feature enables improved type checking of your code.
  */
 class TypeAnnotations {
   // TODO: Implement TypeAnnotations example code
